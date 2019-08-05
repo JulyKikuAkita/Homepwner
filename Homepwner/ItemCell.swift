9e0395b6e5114cecfc84c23c58f552fd99084358
JulyKikuAkita <i-fang.lee@grindr.com>
2019-08-04 19:59:43 -0700

    chp12 wip - p217 dynamic type

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by July on 8/4/19.
+//  Copyright © 2019 July. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
